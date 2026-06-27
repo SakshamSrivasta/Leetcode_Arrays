f557af0405ea0c3883bfc808acf06496a6aade11
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-06-27 13:21:30 +0530

    Time: 0 ms (100%), Space: 20.4 MB (40.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,7 +2,5 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
         sum1=sum(nums)
-        sum2=0
-        for i in range(0,n+1):
-            sum2+=i
+        sum2=(n*(n+1))//2
         return sum2-sum1
