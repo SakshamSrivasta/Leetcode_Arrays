a6a119caf0a1926c8d136911c495768234982ad3
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-06-28 23:24:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (73.33%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -7,6 +7,6 @@ class Solution:
         if any(c not in counter for c in target):
             return 0
         req= defaultdict(int)
-        for c in target:
+        for c in target:#since we dont have exact target word fixed over here, so we are storing the target word's frequency in this dictionary, and then later we wil devide it with the frequency of letter present in s.
             req[c]+=1
         return min(counter[c]//req[c] for c in req)
