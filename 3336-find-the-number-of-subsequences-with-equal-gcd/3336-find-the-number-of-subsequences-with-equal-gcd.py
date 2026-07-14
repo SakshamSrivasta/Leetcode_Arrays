69600a0969a8d70a920508a40489f71e639a9539
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-14 17:29:09 +0530

    Time: 5631 ms (6.82%), Space: 20.5 MB (90.91%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.py
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.py
@@ -1,36 +1,36 @@
 class Solution:
     def subsequencePairCount(self, nums: List[int]) -> int:
-        MOD = 10 ** 9 + 7
+        from math import gcd
 
-        cur = [[0] * 201 for _ in range(201)]
-        cur[0][0] = 1
+        MOD = 10 ** 9 + 7
+        MAX = 200
+
+        dp = [[0] * (MAX + 1) for _ in range(MAX + 1)]
+        dp[0][0] = 1
 
         for x in nums:
-            nxt = [[0] * 201 for _ in range(201)]
+            ndp = [[0] * (MAX + 1) for _ in range(MAX + 1)]
 
-            for g1 in range(201):
-                for g2 in range(201):
-
-                    if cur[g1][g2] == 0:
+            for g1 in range(MAX + 1):
+                for g2 in range(MAX + 1):
+                    if dp[g1][g2] == 0:
                         continue
 
-                    ways = cur[g1][g2]
+                    ways = dp[g1][g2]
 
-                    # Ignore
-                    nxt[g1][g2] = (nxt[g1][g2] + ways) % MOD
+                    ndp[g1][g2] = (ndp[g1][g2] + ways) % MOD
 
-                    # Put in seq1
                     ng1 = x if g1 == 0 else gcd(g1, x)
-                    nxt[ng1][g2] = (nxt[ng1][g2] + ways) % MOD
+                    ndp[ng1][g2] = (ndp[ng1][g2] + ways) % MOD
 
-                    # Put in seq2
                     ng2 = x if g2 == 0 else gcd(g2, x)
-                    nxt[g1][ng2] = (nxt[g1][ng2] + ways) % MOD
+                    ndp[g1][ng2] = (ndp[g1][ng2] + ways) % MOD
 
-            cur = nxt
+            dp = ndp
 
         ans = 0
-        for g in range(1, 201):
-            ans = (ans + cur[g][g]) % MOD
+
+        for g in range(1, MAX + 1):
+            ans = (ans + dp[g][g]) % MOD
 
         return ans
