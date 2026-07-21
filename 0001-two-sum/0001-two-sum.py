82cddcb051256da4e6e2bcbbf373b7815d5d7c58
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-21 23:47:27 +0530

    Time: 0 ms (100%), Space: 20.4 MB (40.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dictt={}
         for i in range(0,len(nums)):
-            remaining=target- nums[i]
+            remaining=target-nums[i]
             if remaining in dictt:
                 return [dictt[remaining],i]
             dictt[nums[i]]=i
