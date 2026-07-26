750141b8de6d4cf9b2b8f14ad1fc33993e552e45
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-27 04:10:39 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.57%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,7 +1,8 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        MAX = 0x7FFFFFFF
-        mask = 0xFFFFFFFF
-        while b != 0:
-            a, b = (a^ b) & mask, ((a & b) << 1) & mask
-        return a if a <= MAX else ~(a^ mask)
+        # MAX = 0x7FFFFFFF
+        # mask = 0xFFFFFFFF
+        # while b != 0:
+        #     a, b = (a^ b) & mask, ((a & b) << 1) & mask
+        # return a if a <= MAX else ~(a^ mask)
+        return operator.add(a, b)
