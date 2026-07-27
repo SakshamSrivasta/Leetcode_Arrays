71df44432b195405d232a2698c1feb3800ab7927
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-28 00:32:45 +0530

    Time: 3 ms (3.12%), Space: 19.2 MB (45.8%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,3 +1,9 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        result=""
+        for ch in s:
+            if 65 <= ord(ch) <= 90:
+                result+=chr(ord(ch)+32)
+            else:
+                result+=ch
+        return result
