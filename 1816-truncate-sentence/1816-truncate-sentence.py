30b698454c0c9901319d3b13d7d34cd7a27969d5
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-29 10:37:45 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.81%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        words=s.split()
+        op=[]
+        i=0
+        if k==len(words):
+            return s
+        while i<k:
+            op.append(words[i])
+            i+=1
+        return " ".join(op)
+
