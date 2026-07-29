9cc1d57d7d658fd38b8ae91f4de9e1951b2858e0
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-29 10:29:34 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.28%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,11 +1,40 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        #USING 2 POINTER
+        
+        if ch not in word:
+            return word
+        idx=word.index(ch)
+        if idx==-1:
+            return word
+        left,right=0,idx
+        wor=list(word)
+        while left<right:
+            wor[left],wor[right]=wor[right],wor[left]
+            left+=1
+            right-=1
+        return ''.join(wor)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #USING JUST SLICING
         # if ch not in word:
         #     return word
         # idx=word.index(ch)
         # return word[:idx+1][::-1]+word[idx+1:]
-        try:
-            idx=word.index(ch)
-            return word[:idx+1][::-1]+word[idx+1:]
-        except ValueError:
-            return word
+
+        #USING TRY AND EXCEPT
+        # try:
+        #     idx=word.index(ch)
+        #     return word[:idx+1][::-1]+word[idx+1:]
+        # except ValueError:
+        #     return word
+        
