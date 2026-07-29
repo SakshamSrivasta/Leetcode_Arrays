d224535a96ef2d34e0626decbe415c8dfd0996e3
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-29 08:45:55 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.7%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.py
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -1,10 +1,10 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        res=""
+        res=[]
         for ch in s:
             if ch=="i":
                 res=res[::-1]
             else:
-                res+=ch
-        return res
+                res.append(ch)
+        return ''.join(res)
         
