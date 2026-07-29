538b08130d59bda3c52fbee124157c57c52572fe
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-29 08:40:09 +0530

    Time: 3 ms (74.58%), Space: 19.2 MB (49.27%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        res=""
+        for ch in s:
+            if ch=="i":
+                res=res[::-1]
+            else:
+                res+=ch
+        return res
+        
