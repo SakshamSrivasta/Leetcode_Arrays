e68da90ff0bfac0eb47b53c5bbe3fe4158c0365a
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-07-31 07:42:17 +0530

    Time: 63 ms (49.25%), Space: 29.2 MB (32.63%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,5 +1,27 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
+        # window_avg=0
+        # for i in range(k):
+        #     window_avg+=nums[i]
+        # max_avg=window_avg
+        # for i in range(k,len(nums)):
+        #     window_avg+=nums[i]
+        #     window_avg-=nums[i-k]
+        #     max_avg=max(max_avg,window_avg)
+        # return max_avg/k
+
+
+
+
+
+
+
+
+
+
+
+
+
         window_avg=0
         for i in range(k):
             window_avg+=nums[i]
@@ -9,3 +31,4 @@ class Solution:
             window_avg-=nums[i-k]
             max_avg=max(max_avg,window_avg)
         return max_avg/k
+            
