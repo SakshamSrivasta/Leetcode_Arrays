014aa09f25e26e94eda8f0d93862a894f92606ab
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-08-04 11:10:19 +0530

    Time: 10 ms (70.2%), Space: 20.8 MB (45.29%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        dictt={}
+        for c in nums:
+            dictt[c]=dictt.get(c,0)+1
+        for key in dictt:
+            if dictt[key]==2:
+                repeating_number=key
+        n=len(nums)
+        s1=(n*(n+1))/2
+        s2=sum(nums)-repeating_number
+        return [repeating_number,int(s1-s2)]
+
+
+        #         dictt={}
+        # for c in arr:
+        #     dictt[c]=dictt.get(c,0)+1
+        # for key in dictt:
+        #     if dictt[key] == 2:
+        #         repeating_number = key
+        # n=len(arr)
+        # sum2=n*(n+1)//2
+        # sum1=sum(arr)-repeating_number
+        # return [repeating_number,sum2-sum1]
