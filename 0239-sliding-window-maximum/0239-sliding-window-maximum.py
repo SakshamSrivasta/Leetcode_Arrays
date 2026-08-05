c208b6c32c3209231ce89a1bbe22b7d32a83e485
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-08-06 00:35:02 +0530

    Time: 187 ms (63.35%), Space: 34.5 MB (92.09%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        #BRUTE FORCE
+        # n=len(nums)
+        # ans=[]
+        # if k>n:
+        #     return [max(nums)]
+        # for i in range(n-k+1):
+        #     window_max=float("-inf")
+        #     for j in range(i,i+k):
+        #         if nums[j]>window_max:
+        #             window_max=nums[j]
+        #     ans.append(window_max)
+        #  return ans
+
+        #OPTIMAL- MONOTONIC DECREASING DEQUE
+        dq=deque()
+        ans=[]
+        for i in range(len(nums)):
+            while dq and dq[0]<=i-k:
+                dq.popleft()
+            while dq and nums[i]>nums[dq[-1]]:
+                dq.pop()
+            dq.append(i)
+            if i>=k-1:
+                ans.append(nums[dq[0]])
+        return ans
+
+    
+      
