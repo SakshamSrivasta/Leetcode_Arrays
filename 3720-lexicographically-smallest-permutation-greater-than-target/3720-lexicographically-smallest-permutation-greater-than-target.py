f9b4b4371d0e0e623e4ba9aa6590faaf36a94869
Saksham Srivastava <149704928+SakshamSrivasta@users.noreply.github.com>
2026-08-27 10:01:27 +0530

    Time: 6 ms (92.11%), Space: 19.4 MB (42.11%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -8,30 +8,40 @@ class Solution:
         for ch in target:
             cnt[ord(ch) - ord('a')] -= 1
 
+        bad = 0
+        mask = 0
+
+        for c in range(26):
+            if cnt[c] < 0:
+                bad += 1
+            elif cnt[c]:
+                mask |= 1 << c
+
         for i in range(len(target) - 1, -1, -1):
             cur = ord(target[i]) - ord('a')
             cnt[cur] += 1
 
-            # target[:i] cannot be formed.
-            if any(x < 0 for x in cnt):
+            if cnt[cur] == 0:
+                bad -= 1
+            elif cnt[cur] == 1:
+                mask |= 1 << cur
+
+            if bad:
                 continue
 
-            # Find the smallest character greater than target[i].
-            nxt = -1
-            for c in range(cur + 1, 26):
-                if cnt[c]:
-                    nxt = c
-                    break
-
-            if nxt == -1:
+            # Check whether a character greater than cur exists.
+            up = mask >> (cur + 1)
+            if not up:
                 continue
+
+            # Lowest set bit = smallest available larger character.
+            nxt = cur + 1 + (up & -up).bit_length() - 1
 
             cnt[nxt] -= 1
 
             ans = list(target[:i])
             ans.append(chr(nxt + ord('a')))
 
-            # Put the remaining characters in sorted order.
             for c in range(26):
                 ans.extend(chr(c + ord('a')) * cnt[c])
 
